da6132b29ca591fbfa2fca4f826a7c66eef6186d
cmsbuild <>
2011-02-05 00:58:34 +0000

    This commit was manufactured by cvs2git to create tag 'RecoEgamma- EgammaPhotonProducers-V08-02-01-01'.
    Sprout from master 2011-01-31 11:24:40 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'compiler warnings'
    Cherrypick from master 2011-02-05 00:58:32 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Back up to 4-2_X and move delete to right place':
        src/ConvertedPhotonProducer.cc

--- src/ConvertedPhotonProducer.cc
+++ src/ConvertedPhotonProducer.cc
@@ -110,6 +110,7 @@ ConvertedPhotonProducer::ConvertedPhotonProducer(const edm::ParameterSet& config
 
 ConvertedPhotonProducer::~ConvertedPhotonProducer() {
   delete theTrackPairFinder_;
+  delete theLikelihoodCalc_;
   delete theVertexFinder_;
 }
 
@@ -130,7 +131,6 @@ void  ConvertedPhotonProducer::beginRun (edm::Run& r, edm::EventSetup const & th
 
 
 void  ConvertedPhotonProducer::endRun (edm::Run& r, edm::EventSetup const & theEventSetup) {
-  delete theLikelihoodCalc_;
 }
 
 
